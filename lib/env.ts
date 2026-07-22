5acbccabe915e2b8ca497c69df0305ccfb704f7b
szamøca <szamosidani@gmail.com>
2026-07-22 14:35:41 +0200

    Set up drizzle + turos

--- lib/env.ts
+++ lib/env.ts
@@ -3,7 +3,9 @@ import { z } from "zod";
 import tryParseEnv from "./try-parse-env";
 
 const EnvSchema = z.object({
-  NODE_ENV: z.string(),
+  NODE_ENV: z.enum(["development", "stage", "production"]),
+  TURSO_DATABASE_URL: z.string(),
+  TURSO_AUTH_TOKEN: z.string(),
 });
 
 export type EnvSchema = z.infer<typeof EnvSchema>;

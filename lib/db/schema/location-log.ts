0b4b5cfe4282bff74955179d04a2cadb6c5db8da
Dániel Szamosi <szamosidani@gmail.com>
2026-07-23 13:08:59 +0200

    Set up better-auth (#54)

--- lib/db/schema/location-log.ts
+++ lib/db/schema/location-log.ts
@@ -1,5 +1,6 @@
 import { int, real, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
+import { user } from "./auth";
 import { location } from "./location";
 
 export const locationLog = sqliteTable("locationLog", {
@@ -12,6 +13,7 @@ export const locationLog = sqliteTable("locationLog", {
   latitude: real().notNull(),
   longitude: real().notNull(),
   locationId: int().notNull().references(() => location.id),
+  userId: int().notNull().references(() => user.id),
   createdAt: int().notNull().$default(() => Date.now()),
   updatedAt: int().notNull().$default(() => Date.now()).$onUpdate(() => Date.now()),
 });

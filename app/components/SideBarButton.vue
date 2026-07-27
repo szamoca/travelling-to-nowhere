b2542fddff2555541c228ba0454c13f4706c14f9
Dániel Szamosi <szamosidani@gmail.com>
2026-07-27 19:38:55 +0200

    Implement side bar navigation (#59)
    * Add static sidebar
    
    * Implement sidebar behaviour
    
    * Persist sidebar state from local storage

---
+++ app/components/SideBarButton.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+const { label, icon, href, showLabel } = defineProps<{
+  label: string;
+  icon: string;
+  href: string;
+  showLabel: boolean;
+}>();
+
+const { path } = useRoute();
+</script>
+
+<template>
+  <div
+    class="tooltip-right"
+    :data-tip="showLabel ? undefined : label"
+    :class="{ tooltip: !showLabel }"
+  >
+    <NuxtLink
+      :class="{ 'text-secondary': path === href, 'justify-start': showLabel, 'justify-center': !showLabel }"
+      class="flex gap-2 p-2 hover:bg-base-300 hover:cursor-pointer flex-nowrap"
+      :to="href"
+    >
+      <Icon :name="icon" size="24" />
+      <Transition name="grow">
+        <span v-if="showLabel">
+          {{ label }}
+        </span>
+      </Transition>
+    </NuxtLink>
+  </div>
+</template>
+
+<style scoped>
+.grow-enter-active {
+  animation: grow 0.2s;
+}
+.grow-leave-active {
+  animation: grow 0.2s reverse;
+}
+@keyframes grow {
+  0% {
+    transform: scale(0);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+</style>
--- app/pages/dashboard/index.vue
+++ app/pages/dashboard/index.vue
@@ -1,5 +1,59 @@
+<script setup lang="ts">
+const isSideBarOpen = ref(true);
+
+function toggleSideBar() {
+  isSideBarOpen.value = !isSideBarOpen.value;
+  localStorage.setItem("isSideBarOpen", isSideBarOpen.value.toString());
+}
+
+onMounted(() => {
+  isSideBarOpen.value = localStorage.getItem("isSideBarOpen") === "true";
+});
+</script>
+
 <template>
-  <h1>
-    You are now logged in!
-  </h1>
+  <div class="flex-1 flex">
+    <div class="bg-base-200 transition-all duration-300" :class="{ 'w-64': isSideBarOpen, 'w-12': !isSideBarOpen }">
+      <div
+        class="flex hover:cursor-pointer hover:bg-base-300 p-1"
+        :class="{ 'justify-center': !isSideBarOpen, 'justify-end': isSideBarOpen }"
+        @click="toggleSideBar"
+      >
+        <Icon
+          v-if="isSideBarOpen"
+          name="pixelarticons:square-chevron-left"
+          size="32"
+        />
+        <Icon
+          v-else
+          name="pixelarticons:square-chevron-right"
+          size="32"
+        />
+      </div>
+      <div class="flex flex-col gap-1">
+        <SideBarButton
+          label="Locations"
+          icon="pixelarticons:map-pin"
+          href="/dashboard"
+          :show-label="isSideBarOpen"
+        />
+        <SideBarButton
+          label="Add location"
+          icon="pixelarticons:image-plus"
+          href="/dashboard/add"
+          :show-label="isSideBarOpen"
+        />
+        <div class="divider" />
+        <SideBarButton
+          label="Sign out"
+          icon="pixelarticons:logout"
+          href="/sign-out"
+          :show-label="isSideBarOpen"
+        />
+      </div>
+    </div>
+    <div class="flex-1">
+      Content
+    </div>
+  </div>
 </template>

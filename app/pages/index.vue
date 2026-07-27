7d94b03f60379b495bca3fc87dbb58dc1befd9c0
szamøca <szamosidani@gmail.com>
2026-07-27 17:34:06 +0200

    Use protected routes for dashboard

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+const authStore = useAuthStore();
+</script>
+
 <template>
   <div class="hero bg-base-300 container mx-auto mt-4">
     <div class="hero-content text-center min-h-96">
@@ -8,7 +12,14 @@
         <p class="py-6">
           This is a travel log for all the exciting things you've done in your life so far. Add places, events, and memories. Let's get going!
         </p>
-        <AuthButton />
+        <AuthButton v-if="!authStore.user" />
+        <NuxtLink
+          v-else
+          to="/dashboard"
+          class="btn btn-primary"
+        >
+          Start logging
+        </NuxtLink>
       </div>
     </div>
   </div>

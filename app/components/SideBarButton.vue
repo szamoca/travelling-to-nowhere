be8f35e4c11406473792008af61957c5b6cff0c7
szamøca <szamosidani@gmail.com>
2026-07-27 19:04:26 +0200

    Add static sidebar

---
+++ app/components/SideBarButton.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const { label, icon } = defineProps<{
+  label: string;
+  icon: string;
+  href: string;
+}>();
+
+const { path } = useRoute();
+</script>
+
+<template>
+  <NuxtLink
+    :class="{ 'text-secondary': path === href }"
+    class="flex justify-start gap-2 p-2 hover:bg-base-300 hover:cursor-pointer"
+    :to="href"
+  >
+    <Icon :name="icon" size="24" />
+    {{ label }}
+  </NuxtLink>
+</template>
--- app/pages/dashboard/index.vue
+++ app/pages/dashboard/index.vue
@@ -1,5 +1,30 @@
 <template>
-  <h1>
-    You are now logged in!
-  </h1>
+  <div class="flex-1 flex">
+    <div class="bg-base-200 w-64">
+      <div class="flex justify-end">
+        <Icon name="pixelarticons:square-chevron-left" size="32" />
+      </div>
+      <div class="flex flex-col gap-1">
+        <SideBarButton
+          label="Locations"
+          icon="pixelarticons:map-pin"
+          href="/dashboard"
+        />
+        <SideBarButton
+          label="Add location"
+          icon="pixelarticons:image-plus"
+          href="/dashboard/add"
+        />
+        <div class="divider" />
+        <SideBarButton
+          label="Sign out"
+          icon="pixelarticons:logout"
+          href="/sign-out"
+        />
+      </div>
+    </div>
+    <div class="flex-1">
+      Content
+    </div>
+  </div>
 </template>

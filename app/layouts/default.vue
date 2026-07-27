d1a771713723f8d52093eaeb2015774e4ccf3032
szamøca <szamosidani@gmail.com>
2026-07-27 18:27:24 +0200

    Pass auth from SSR

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+// I don't exactly understand this part and also do not get the desired SSR results like in the crash course so I might need to be looking into this more
+const authStore = useAuthStore();
+await authStore.init();
+</script>
+
 <template>
   <div class="flex min-h-screen flex-col">
     <AppNavBar />
